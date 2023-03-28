6394cf9354d882c920dd4e87e840c2484bd64dcf
ShamrockCode <1360498907@qq.com>
2023-03-28 16:17:50 +0800

    test1

---
+++ test/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @Author Shamrock
+ * @Date ${DATE} ${TIME}
+ * @description: ${description}
+ * @Title: ${NAME}
+ * @Package
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a test!!");
+    }
+}
